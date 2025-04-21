d3da77e603895467cb6910b8b084453932f14f01
Costa <117647243+Jaca7x@users.noreply.github.com>
2025-04-21 01:13:37 -0300

    Delete kongRunner directory

--- kongRunner/screens/collision.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef COLLISION_H
-#define COLLISION_H
-
-void collision(int meters, int screenHeight, int screenWidth);
-
-#endif
--- kongRunner/utils/map.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef MAP_H
-#define MAP_H
-
-void updateMap(float *groundX1, float *groundX2, float scrollSpeed, float delta, int bgWidth);
-
-#endif
